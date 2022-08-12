c10a074ae61a29719e686361071f61cbf99fefd8
marcelo-betassa <3986286+marcelob-tech@users.noreply.github.com>
2022-08-12 17:11:58 -0300

    desafio angular user-list

--- src/app/shared/user-list/components/user-card/user-card.component.ts
+++ src/app/shared/user-list/components/user-card/user-card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-card',
-  templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+	selector: 'app-user-card',
+	templateUrl: './user-card.component.html',
+	styleUrls: ['./user-card.component.scss'],
 })
 export class UserCardComponent implements OnInit {
+	@Input() card: any;
+	formUser: FormGroup;
 
-  constructor() { }
+	constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit() {
+		this.formUser = this.fb.group({
+			selected: [false],
+		});
+	}
 }
--- src/app/shared/user-list/user-list.component.ts
+++ src/app/shared/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,9 +8,43 @@ import { Router } from '@angular/router';
 	styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
-	constructor(private router: Router) {}
+	cardList: any[];
+	formUserList: FormGroup;
+	constructor(private router: Router, private changeDetector: ChangeDetectorRef, private fb: FormBuilder) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.formUserList = this.fb.group({
+			checkAll: [false],
+		});
+		this.cardList = [
+			{
+				id: 1651,
+				imgUrl: '',
+				nome: 'xpto',
+				email: 'mhbeta@gmail.com',
+				emailSecundario: 'xptotest@gmail.com',
+				dataAdicionado: '2022-08-11T23:57:08.521Z',
+			},
+			{
+				id: 2979,
+				imgUrl: 'C:\\fakepath\\moboxAvatar.png',
+				nome: 'xptozxy',
+				email: 'mhbeta@gmail.com',
+				emailSecundario: 'xpblabla@gmail.com',
+				dataAdicionado: '2022-08-12T00:02:33.620Z',
+			},
+			{
+				id: 937,
+				imgUrl: '',
+				nome: 'xpto',
+				email: 'mhbeta@gmail.com',
+				emailSecundario: 'fulanoxpto@outlook.com',
+				dataAdicionado: '2022-08-12T02:23:20.198Z',
+			},
+		];
+
+		this.changeDetector.detectChanges();
+	}
 
 	navigateToUserForm() {
 		this.router.navigateByUrl('user-form');
